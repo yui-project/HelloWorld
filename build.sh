40c4ba05bffe2c2350774a588467355db813f266
Ryo Abe <rabe.scor.esys@gmail.com>
2019-10-09 18:40:02 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-arduino-cli compile --fqbn arduino:avr:mega:cpu=atmega2560  Test
+arduino-cli compile --fqbn arduino:avr:mega:cpu=atmega2560  HelloWorld
 
-arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega:cpu=atmega2560 Test
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega:cpu=atmega2560 HelloWorld
