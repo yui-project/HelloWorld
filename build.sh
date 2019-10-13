06c8f1e03fa4042dd921b529bc10cc0c78b9835b
Ryo Nakagawa <funanori9292@gmail.com>
2019-10-13 17:09:59 +0900

    Fixed working directory

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 arduino-cli lib install ArduinoUnit
 
+cd src/
+
+echo "cd src/"
+
 arduino-cli compile --fqbn arduino:avr:mega:cpu=atmega2560  HelloWorld
 
 arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega:cpu=atmega2560 HelloWorld
