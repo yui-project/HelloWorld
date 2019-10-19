7010c95c2d2713f25a72d5e75f3580c7c090cec4
Ryo Abe <rabe.scor.esys@gmail.com>
2019-10-19 13:27:36 +0900

    add poetry files

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 io_test() {
     echo IO_TEST : $1
     arduino-cli compile --fqbn arduino:avr:mega:cpu=atmega2560 $1
-    pytest $1.py
+    poetry run pytest $1.py
 }
 
 for dir_path in `\find ./test/IO -maxdepth 1 -mindepth 1 -type d`; do
